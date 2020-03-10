d846e07e1c5c16bb1185b31b690d39546b188ac9
Deepak Kumar <deepakkumar@gmail.com>
2020-03-10 13:06:32 +0530

    error handling

--- app/controllers/BodyParserController.scala
+++ app/controllers/BodyParserController.scala
@@ -6,19 +6,21 @@ import play.api.libs.json.JsValue
 import play.api.mvc._
 import java.io.File
 
-
 @Singleton
-class BodyParserController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class BodyParserController @Inject() (
+    val controllerComponents: ControllerComponents
+) extends BaseController {
 
   // JSON content coming in with Content-Type set to "application/json"
   def jsonContent = Action { request: Request[AnyContent] =>
     val jsonBody: Option[JsValue] = request.body.asJson
 
     jsonBody
-      .map { json =>
-        Ok("Got: " + (json \ "name").as[String])
-      }.getOrElse {
-        BadRequest("Expecting json content with Content-Type header set to application/json")
+      .map { json => Ok("Got: " + (json \ "name").as[String]) }
+      .getOrElse {
+        BadRequest(
+          "Expecting json content with Content-Type header set to application/json"
+        )
       }
   }
 
@@ -40,19 +42,19 @@ class BodyParserController @Inject()(val controllerComponents: ControllerCompone
     Ok("I am not sure how to do this")
   }
 
-  def explicitJsonParser = Action(parse.json) { implicit request: Request[JsValue] =>
-    Ok("We got some json content: " + (request.body \ "name").as[String])
+  def explicitJsonParser = Action(parse.json) {
+    implicit request: Request[JsValue] =>
+      Ok("We got some json content: " + (request.body \ "name").as[String])
   }
 
   //  Uploading a file
   private val parseFileInput = parse.using { request =>
     request.session
       .get("username")
-      .map { name =>
-        parse.file(new File("/tmp/" + name + ".upload"))
-      }.getOrElse {
-      sys.error("You are not logged to to do that")
-    }
+      .map { name => parse.file(new File("/tmp/" + name + ".upload")) }
+      .getOrElse {
+        sys.error("You are not logged to to do that")
+      }
   }
 
   def uploadFile = Action(parseFileInput) { implicit request =>
